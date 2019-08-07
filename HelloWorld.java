a8e1b588a0130bcaad146a86d64d03fa2ea97665
zhangliancheng <2495489706@qq.com>
2019-08-07 20:03:03 +0800

    八月七日提交

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		Num(10,50);
+	}
+	public static void Num(int Num1, int Num2){
+		int result = Num1+Num2;
+		// return result;
+		System.out.print(result);
+	}
+}
