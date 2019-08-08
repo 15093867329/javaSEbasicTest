058dde02c18a6a936cef32dbc6a38adb6654a9f3
15093867329 <53028576+15093867329@users.noreply.github.com>
2019-08-08 16:43:29 +0800

    提交0808第一次

---
+++ Task.java
@@ -0,0 +1,12 @@
+package com.neuedu.test;
+
+import java.util.Random;
+
+public class Task {
+	public static void main(String[] args) {
+		Random random = new Random();
+		int nextInt = random.nextInt(100);
+		System.out.print(nextInt);
+	}
+	
+}
